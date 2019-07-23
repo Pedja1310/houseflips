8168d21d1119e21033daf1f1d23f52bfc558f249
Pedja1310 <pedja1310@gmail.com>
2019-07-23 13:54:43 +0200

    Create first version of  property model.

--- models/Property.js
+++ models/Property.js
@@ -1,7 +1,36 @@
 const mongoose = require('mongoose');
 
 const PropertySchema = new mongoose.Schema({
-  owner: {
-    
+  publisher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  size: {
+    type: Number,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  images: [
+    {
+      type: String,
+      // required: true
+    }
+  ],
+  added: {
+    type: Date,
+    default: Date.now
   }
 });
+
+module.exports = Property = mongoose.model('property', PropertySchema);
