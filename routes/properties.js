e394bd8e874d0c06c77c13c58540f51b46440bca
Pedja1310 <pedja1310@gmail.com>
2019-07-23 20:54:54 +0200

    Finished CRUD functionality form user properties.

--- routes/properties.js
+++ routes/properties.js
@@ -47,6 +47,27 @@ router.post('/new', auth, async (req, res) => {
 
 });
 
+// @route   GET api/properties/:propertyId
+// @desc    get single property
+// @access  public
+router.get('/:propertyId', async (req, res) => {
+  try {
+    const property = await Property.findOne({ _id: req.params.propertyId });
+    
+    if(!property) return res.status(404).json({ msg: "Property not found." });
+    
+    res.json(property);
+  } catch (error) {
+    console.error(error);
+
+    if(error.kind ==='ObjectId') { 
+      return res.status(400).json({ msg: "Profile not found." });
+    }
+
+    res.status(500).json({ msg: "Server error." });
+  }
+});
+
 // @route     PUT api/properties/edit/:propertyId
 // @desc      edit existing property
 // @access    private
@@ -65,6 +86,9 @@ router.put('/edit/:propertyId', auth, async (req, res) => {
 
     if (!property) return res.status(400).json({ msg: "Property not found."});
 
+    if(property.publisher.toString() !== req.user._id) {
+      return res.status(401).json({ msg: "Not Authorized." });
+    }
     
     property = await Property.findOneAndUpdate(
       { _id: req.params.propertyId },
@@ -75,9 +99,34 @@ router.put('/edit/:propertyId', auth, async (req, res) => {
     res.status(200).json(property);
   } catch (error) {
     console.error(error);
-    res.status(500).json('Server error.')
+    res.status(500).json({ msg: 'Server error.' })
   }
+});
 
+// @route   Delete apit/properties
+// @desc    remove property
+// @access  private
+router.delete('/:propertyId', auth, async (req, res) => {
+  try {
+    const property = await Property.findOne({ _id: req.params.propertyId });
+
+    if(property.publisher.toString() !== req.user._id) {
+      return res.status(401).json({ msg: "Not Authorized." });
+    }
+
+    let user = await User.findOneAndUpdate(
+      { _id: req.user._id},
+      { $pull: { properties: req.params.propertyId }},
+      { new: true }  
+    );
+
+    await Property.findOneAndDelete({ _id: req.params.propertyId });
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error."});
+  }
 });
 
 
