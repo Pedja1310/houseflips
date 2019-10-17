d06ffaecd53a17f7c0b2065eafd3169211805020
Pedja1310 <pedja1310@gmail.com>
2019-10-17 14:56:06 +0200

    Added add property component to routes.

---
+++ client/src/components/properties/AddProperty.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AddProperty = props => {
+  return (
+    <div>
+      <h1>Add new Property</h1>
+    </div>
+  );
+};
+
+AddProperty.propTypes = {};
+
+export default AddProperty;
